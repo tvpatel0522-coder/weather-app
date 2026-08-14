8e3ea4a4103f42ea429e0d161493dac99cc4db78
tpatel1142023-eng <tpatel1142023@gmail.com>
2026-08-15 01:54:25 +0530

    fix url and weather function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     // 🔑 Replace this with your new active API Key from weatherapi.com
-    const BACKEND_URL= "https://weather-backend-2aol.onrender.com"; 
+    const API_KEY= "86a19080ff55477a85a182235261308"; 
 
     // DOM Elements
     const startBtn = document.getElementById("start-btn");
